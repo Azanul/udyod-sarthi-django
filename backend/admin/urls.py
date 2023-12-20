b1358a54e9814bf8bd97efc2f69b87f280d8b7e3
Azanul <azanulhaque@gmail.com>
2023-12-20 12:32:56 +0530

    feat: delete admin
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- backend/admin/urls.py
+++
@@ -1,9 +0,0 @@
-# from django.urls import path
-# from .views import SignupView, LoginView
-
-# urlpatterns = [
-#     path('signup/', SignupView.as_view(), name='signup'),
-#     path('login/', LoginView.as_view(), name='login'),
-#     path('api/token/', obtain_auth_token, name='token_obtain_pair'),  # Token endpoint
-#     path('api/token/refresh/', obtain_auth_token, name='token_refresh'),
-# ]
